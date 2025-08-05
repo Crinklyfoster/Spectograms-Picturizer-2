0b173193bd4bdcf4f3ce2a78eb2c0c4a3063d636
Snehil_007_ <96987907+Crinklyfoster@users.noreply.github.com>
2025-08-05 16:17:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -459,4 +459,6 @@ def internal_error(error):
                          max_files=MAX_FILES_PER_SESSION), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+    
